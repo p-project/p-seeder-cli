a0a236254b6380cbd30d6c906744359e6c3ad6fa
IGI-111 <igi-111@protonmail.com>
2017-03-20 20:50:17 +0000

    rename print to fail

--- src/error.js
+++ src/error.js
@@ -3,22 +3,22 @@ import { daemon } from './daemon'
 
 export function handleError (e) {
   if (e.name === 'RequestError') {
-    print(`Could not contact server at ${daemon}`)
+    fail(`Could not contact server at ${daemon}`)
   } else if (e.name == 'StatusCodeError') {
     try {
       const error = isObject(e.error) ? e.error.message : JSON.parse(e.error).message
-      print(error)
+      fail(error)
     } catch(err) {
-      print(e.message)
+      fail(e.message)
     }
   } else {
-    print(e.message)
+    fail(e.message)
   }
-  process.exit(1)
 }
 
-function print (err) {
+function fail (err) {
   console.error(chalk.red(err))
+  process.exit(1)
 }
 
 function isObject(val) {
